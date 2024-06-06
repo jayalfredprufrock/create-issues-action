4dc64f7c2dd924edfc433553701b533cbfecc624
jayalfredprufrock <jayalfredprufrock@gmail.com>
2024-06-05 21:57:58 -0400

    fix: test creating project items

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as glob from '@actions/glob';
+import GhProjectsApi from "github-project";
 import yaml from "js-yaml";
 import fs from 'fs/promises';
 import path from 'path';
@@ -10,9 +11,12 @@ import { titleCase } from './util.js';
 const templatePath = core.getInput('template-path');
 const followSymbolicLinks = core.getBooleanInput('follow-symbolic-links');
 const githubToken = core.getInput('github-token') ?? process.env.GITHUB_TOKEN ?? '';
+const projectNumberDefault = core.getInput('project-number');
 
 const globber = await glob.create(templatePath, { followSymbolicLinks });
 const templateFiles = await globber.glob();
+
+const octokit = github.getOctokit(githubToken);
 
 const frontmatterRegex = /^\s*-{3,}\s*$/m;
 
@@ -23,15 +27,27 @@ export const processTemplateFile = async (templateFile: string): Promise<void> =
 
     const issueData = (yamlData ? yaml.load(yamlData) : {}) as Record<string, any>;
     
-    const octokit = github.getOctokit(githubToken);
-
-    const issue = await octokit.rest.issues.create({
+    const { data: issue } = await octokit.rest.issues.create({
         ...github.context.repo,
         title: issueData.title ?? titleCase(path.parse(templateFile).name),
         labels: issueData.labels,
         assignees: issueData.assignees,
+        milestone: issueData.milestone,
         body
     });
+    
+    const projectNumber = projectNumberDefault ?? issueData.projectNumber;
+
+    if (projectNumber) {
+        const ghProjectsApi = new GhProjectsApi({
+            octokit: octokit as any,
+            owner: github.context.repo.owner,
+            number: parseInt(projectNumber),
+            token: githubToken
+        });
+
+        const projectItem = await ghProjectsApi.items.add(issue.node_id);
+    }
 }
 
 await Promise.all(templateFiles.map(templateFile => processTemplateFile(templateFile)));
