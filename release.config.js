61defab07acc5c420ab6830da064eb042215c96f
jayalfredprufrock <jayalfredprufrock@gmail.com>
2024-06-07 10:26:03 -0400

    fix: auto set package.json version when releasing

--- release.config.js
+++ release.config.js
@@ -3,8 +3,9 @@ const config = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
       ["@semantic-release/git", {
-        "assets": ["dist/*.js"],
+        "assets": ["package.json", "dist/index.js"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }],
       '@semantic-release/github',
